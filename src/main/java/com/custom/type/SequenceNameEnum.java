f1c9f5431a1e664008d5a2cc9dad8b22b5fa1a21
robin.tian <robin.tianze@gmail.com>
2016-04-05 17:47:39 +0800

    尝试调用存储过程

--- src/main/java/com/custom/type/SequenceNameEnum.java
+++ src/main/java/com/custom/type/SequenceNameEnum.java
@@ -1,7 +1,43 @@
 package com.custom.type;
 
+import com.custom.common.BaseEnum;
+import org.apache.commons.lang.StringUtils;
+
 /**
  * Created by robin.tian on 16-3-31.
  */
-public enum SequenceNameEnum {
+public enum SequenceNameEnum implements BaseEnum {
+    ;
+
+
+    private String code;
+
+    private String desc;
+
+    private SequenceNameEnum getFromCode(String code) {
+        for (SequenceNameEnum nameEnum : values()) {
+            if (StringUtils.equals(code, nameEnum.getCode())) {
+                return nameEnum;
+            }
+        }
+        return null;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public SequenceNameEnum setCode(String code) {
+        this.code = code;
+        return this;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public SequenceNameEnum setDesc(String desc) {
+        this.desc = desc;
+        return this;
+    }
 }
